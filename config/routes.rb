5fc25a014f6e7c20ad3b34c7f574a2a7e6819f5c
Ben Greenberg <bengreenberg@gmail.com>
2021-12-17 12:56:15 +0200

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'demo/show'
+  get 'demo', to: 'demo#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "demo#index"
+end
